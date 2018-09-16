0281da5274ad60b4d49e1c2c4e8b16e81b4845f3
https://github.com/pxmei/work.git <1518056096@qq.com>
2018-09-16 18:17:02 +0800

    0204 homework1

--- untitled1/main.cpp
+++ untitled1/main.cpp
@@ -15,30 +15,35 @@ typedef quint8 byte;                                     //定义word为32位 by
 int main(int argc, char *argv[])
 {
     qint32 i=0x12345678;                                 //定义一个32位无符号整型数
-    qDebug() << "原始值：0x12345678==" << i;              //输出原始值i
-    qDebug() << "0x12==" << WORD_HHI(i)                  //输出最高8位
-             << "0x34==" << WORD_LHI(i)                  //输出次高8位
-             << "0x56==" << WORD_HLO(i)                  //输出次低8位
-             << "0x78==" << WORD_LLO(i);                 //输出最低8位
+    qDebug("原始值：0x%x==%d",i,i) ;                      //输出原始值i
+    qDebug("0x%x==%d  0x%x==%d  0x%x==%d  0x%x==%d" ,
+           WORD_HHI(i),WORD_HHI(i),                      //输出最高8位
+           WORD_LHI(i),WORD_LHI(i),                      //输出次高8位
+           WORD_HLO(i),WORD_HLO(i),                      //输出次低8位
+           WORD_LLO(i),WORD_LLO(i));                     //输出最低8位
     QList<qint8> values;
     values << WORD_HHI(i)
            << WORD_LHI(i)
            << WORD_HLO(i)
            << WORD_LLO(i);                               //将取出结果存入values中
 
-    qDebug() << "最高8位和次高8位最大值："
-             << MAX(WORD_HHI(i),WORD_LHI(i));            //输出最高8位和次高8位最大值
-    qDebug() << "次低8位和最低8位最小值："
-             << MIN(WORD_HLO(i),WORD_LLO(i));            //输出最高8位和次高8位最小值
+    qDebug("最高8位和次高8位最大值：0x%x(%d)",
+           (MAX(WORD_HHI(i),WORD_LHI(i))),
+           (MAX(WORD_HHI(i),WORD_LHI(i))));              //输出最高8位和次高8位最大值
+    qDebug("次低8位和最低8位最小值：0x%x(%d)",
+           (MIN(WORD_HLO(i),WORD_LLO(i))),
+           (MIN(WORD_HLO(i),WORD_LLO(i))));              //输出次低8位和最低8位最小值
 
     qint32 j=(WORD_HLO(i) << 24)
             +(WORD_HHI(i) << 16)
             +(WORD_LLO(i) << 8)
-            +(WORD_LHI(i));                              //重新排列组合
-    qDebug() << "重新组合后数值（十进制）：0x56127834=="
-             << j;
-    qDebug() << "重新组合后数值（十六进制）：0x56127834=="
-             << j;
+            +(WORD_LHI(i));                              //重新排列组合成一个新的32位无符号整型数
+    qDebug("重新组合后数值：0x%x(%d)",j,j);                 //输出
+
+    qDebug() << "排序前：" << values;
+    std::sort(values.begin(),values.end(),
+              std::greater<quint8>());                    //使用std::sort函数，将数字从大到小排列
+    qDebug() << "排序后：" << values;
 
     QCoreApplication a(argc, argv);
 
